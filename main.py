3bc3e0cff208681652ea4bd82702ccd349abce06
e <ipbkoa20@gmail.com>
2021-06-03 20:49:42 +0200

    displayng filtered unions

--- main.py
+++ main.py
@@ -21,16 +21,20 @@ def check_len_file(source_of_lines):
     # display_all_cluster_properties(clusters)
     cluster_unions = divide_all_clusters_into_unions(thesaurus)
 
-    # display_unions(cluster_unions)
     # print("num unions:", len(cluster_unions))
     # print("num clusters:",len(clusters))
     filtered_clusters = filter_unions_by_num_clusters(cluster_unions, 4)
-    print("num unions with over 3 clusters:",len(filtered_clusters))
+    print("num unions with over 3 clusters:", len(filtered_clusters))
+    display_unions(filtered_clusters)
 
 
-def filter_unions_by_num_clusters(cluster_unions,num):
+def display_unions(unions):
+    for union in unions:
+        print(union.get_union_description())
+
+
+def filter_unions_by_num_clusters(cluster_unions, num):
     return [u for u in cluster_unions if u.get_num_clusters() >= num]
-
 
 
 def get_some_lines(lines, number_of_entries):
@@ -80,7 +84,7 @@ def divide_all_clusters_into_unions(thesaurus):
     while not thesaurus.is_empty() and len(all_unions) < 2000:
         len_thes = len(thesaurus.all_clusters)
         len_unions = len(all_unions)
-        print("remaining words:",len_thes,";num unions:",len_unions,";total:",len_thes+len_unions)
+        print("remaining words:", len_thes, ";num unions:", len_unions, ";total:", len_thes + len_unions)
         cluster = thesaurus.eject_cluster()
         sort_cluster(cluster, all_unions)
     return all_unions
@@ -121,6 +125,9 @@ class ClusterUnion:
     def matches_cluster(self, cluster):
         return next((True for c in self.clusters if c.has_common_words(cluster)), False)
 
+    def get_union_description(self):
+        return "NEW UNION:\n" + '\n'.join([('*' + cluster.get_entry_description()) for cluster in self.clusters])
+
 
 class Cluster:
     def __init__(self, words):
@@ -141,4 +148,4 @@ class Cluster:
 
 if __name__ == '__main__':
     check_len_file(get_file_lines)
-    #check_len_file(get_dummy_lines)
+    # check_len_file(get_dummy_lines)
